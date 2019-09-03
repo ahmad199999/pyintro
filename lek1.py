9f0fb9aeb83928144bb71f17db8e84dfc2aa58ff
ahmad199999 <ahmad.mirzad@elev.ga.ntig.se>
2019-09-03 10:09:09 +0200

    första 80 minuterna på tisdag 03/09

--- lek1.py
+++ lek1.py
@@ -1,2 +1,11 @@
 print("Hej på er i EE17")
 print("hej hej")
+firstName = "Jens"
+lastName = 'Andersson'
+dataofBirth = 19770815
+age = 42 
+teacher = True
+
+
+print(firstName , lastName + str(age) + str(teacher))
+print(dataofBirth)
