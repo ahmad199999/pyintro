5ecc07230b1a9fde6e052a0e83b06a53867af1ce
ahmad199999 <ahmad.mirzad@elev.ga.ntig.se>
2019-09-03 10:54:26 +0200

    save this

---
+++ Lek2.py
@@ -0,0 +1,13 @@
+# variabler för att komma ihåg och namn och ålder
+# läsa in namn
+# läsa in ålder
+print("Hej och välkommen")
+
+name = input("Vad hetter du: ")
+
+age = int(input("Hur gammal är du "))
+# skriva ut hej namn och du är x år gammal
+# skriv ut om du är 18 eller inte
+print("hajsan", Ahmad,"vällkommen. ")
+
+print("ahmad",19980411, "21")
