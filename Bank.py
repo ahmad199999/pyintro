5eb7e1088fcf4ff06140e0636fc300b20bda66be
ahmad199999 <ahmad.mirzad@elev.ga.ntig.se>
2019-10-08 10:57:30 +0200

    kommentar till banken

--- Bank.py
+++ Bank.py
@@ -9,37 +9,40 @@ town = input("where is your hometown?:") # Input en funktion som frågar använd
 if town != hometown: # En vilkor satts som säkerställer om användaren skriver fel hemstaden logas den ut med hjälp exit().
     exit()
 
-age = 21
-userage = int(input("how old are you?:"))
-if userage != age:
+age = 21 # En variabel som innehåller ålder till användaren
+userage = int(input("how old are you?:")) # Input och int som frågar användare att skriva sin ålder med heltal.
+if userage != age: # Vilkorsatts som jämför användarens ålder.
     exit()
 
-bank = "Swedbank"
-userbank = input("Which bank do you want to contact?:")
+bank = "Swedbank" # En sträng variabel
+userbank = input("Which bank do you want to contact?:") # Variabel och funktion som vill fråga vilken bank vill användare ansluta sig till.
 if userbank != bank:
     exit()
 
 
 pin = 1234 # En integer eller heltal variabel som innehåller lösenord till användaren som vill loga in i programmet.
 userPin = int(input("write in your password:")) # int och input eller en heltal och en funktion som båda vill underlätta fär användaren
-if pin != userPin:                                                                              # skriva in lösenordet med heltal
+if pin != userPin:                                      # skriva in lösenordet med heltal
+ # en vilkorsatts som lösenordet som finns inne i datoren minne   
+  # med den som användaren skriver in och om båda lösenord är samma går användaren till nästa steg annurs logar ut användaren.  
     exit()    
-print("Dear Ahmad Mirzad wellcome to your bank!")
+print("Dear Ahmad Mirzad wellcome to your bank!") # Skriver ut ett meddelande till användaren.
 
 try:
-    with open("balance.txt", "r")as balanceFile:
+    with open("balance.txt", "r")as balanceFile: # vill läsa en fil som heter banlance.txt med hjälp av ("r")
         try:
-            balance = balanceFile.readline()
+            balance = balanceFile.readline()  
             balance = float(balance)
-        except (ValueError):
+        except (ValueError): # try och except är för felhantering. 
             print("File corrupt")
-            balance = 0.0
+            balance = 0.0 # om det inte finns en fil med namnen balance.txt då sättas balance till 0.0
 except (FileNotFoundError):
+    print("FIlen fanns inte") 
     balance = 0.0
 menu = 0
 
-while menu != 3:
-    print("Ditt saldo är:", balance)
+while menu != 3: # while loopen kör runt tills man når 3
+    print("Ditt saldo är:", balance) 
     menu = int(input("skriv ditt val:[1,2,3] "))
     if menu == 1:
         balance = balance + float(input("hur mycket vill du sätta in:"))
@@ -52,8 +55,8 @@ while menu != 3:
         print("Fel eller avslut") 
         print("Fel eller avslut")
         try:
-            with open("balance.txt", "w")as balanceFile:
-                balanceFile.write(str(balance))
+            with open("balance.txt", "w")as balanceFile: # vill skriva balance.txt om det inte finns då skapar den med hjälp av ("w")
+                balanceFile.write(str(balance)) 
         except (FileNotFoundError):
             print("Ingen fil") 
 
